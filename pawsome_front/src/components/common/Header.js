325eb8cc8270deab7e5560947153b28dd28805d5
dhk119 <daehean95@gmail.com>
2024-09-13 18:22:40 +0900

    헤더 css 적용중

--- pawsome_front/src/components/common/Header.js
+++ pawsome_front/src/components/common/Header.js
@@ -1,8 +1,39 @@
+import { Link } from "react-router-dom";
+import "./default.css";
+
 const Header = () => {
   return (
-    <div>
-      <h1>헤더</h1>
-    </div>
+    <header className="header">
+      <div>
+        <div className="logo">
+          <Link to="/">
+            <img
+              src="/image/pawsomelogo.png"
+              style={{ width: "130px", height: "100px" }}
+            />
+          </Link>
+        </div>
+        <MainNavi />
+      </div>
+    </header>
+  );
+};
+
+const MainNavi = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <Link to="#">시설</Link>
+        </li>
+        <li>
+          <Link to="#">마켓</Link>
+        </li>
+        <li>
+          <Link to="#">커뮤니티</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
