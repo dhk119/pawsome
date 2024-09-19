084c7670f6f6411ae30027fa2d8017150ffc2ccd
aemilius <coline672@gmail.com>
2024-09-19 15:49:35 +0900

    Update InquiryWrite.js

--- pawsome_front/src/components/inquiry/InquiryWrite.js
+++ pawsome_front/src/components/inquiry/InquiryWrite.js
@@ -1,4 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { loginEmailState } from "../utils/RecoilData";
+import { useState } from "react";
+
 const InquiryWrite = () => {
+  const navigate = useNavigate();
+  const backServer = process.env.REACT_APP_BACK_SERVER;
+  const [inquiry, setInquiry] = useState({});
+  const [loginEmail, setLoginEmail] = useRecoilState(loginEmailState);
+  const [inquiryTitle, setInquiryTitle] = useState("");
+  const [inquiryContent, setInquiryContent] = useState("");
   return (
     <>
       <div></div>
