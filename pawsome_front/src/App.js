1e4dc97d1acccbdd008b5de54e832861cc4452a7
aemilius <coline672@gmail.com>
2024-09-19 15:05:17 +0900

    Update App.js

--- pawsome_front/src/App.js
+++ pawsome_front/src/App.js
@@ -10,6 +10,7 @@ import Join from "./components/member/Join";
 import Login from "./components/member/Login";
 import AdminMain from "./components/admin/AdminMain";
 import Main from "./components/common/Main";
+import InquiryMain from "./components/inquiry/InquiryMain";
 
 function App() {
   const backServer = process.env.REACT_APP_BACK_SERVER;
@@ -23,6 +24,7 @@ function App() {
           <Route path="/join" element={<Join />} />
           <Route path="/login" element={<Login />} />
           <Route path="/admin/*" element={<AdminMain />} />
+          <Route path="/inquiry/*" element={<InquiryMain />} />
           <Route path="/" element={<Main />} />
         </Routes>
       </main>
