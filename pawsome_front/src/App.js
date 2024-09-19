e9f9c275d71fd03c21640cf0aefe9497e9136c13
aemilius <coline672@gmail.com>
2024-09-19 09:00:31 +0900

    관리자 메인 페이지 기본

--- pawsome_front/src/App.js
+++ pawsome_front/src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import { loginEmailState, memberTypeState } from "./components/utils/RecoilData";
+import {
+  loginEmailState,
+  memberTypeState,
+} from "./components/utils/RecoilData";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import { useRecoilState } from "recoil";
 import Join from "./components/member/Join";
 import Login from "./components/member/Login";
+import AdminMain from "./components/admin/AdminMain";
 
 function App() {
   const backServer = process.env.REACT_APP_BACK_SERVER;
@@ -18,6 +22,7 @@ function App() {
           <Route path="/"></Route>
           <Route path="/join" element={<Join />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/admin/*" element={<AdminMain />} />
         </Routes>
       </main>
       <Footer />
---
+++ pawsome_front/src/components/admin/AdminMain.js
@@ -0,0 +1,8 @@
+const AdminMain = () => {
+  return (
+    <>
+      <div>관리자 페이지 메인</div>
+    </>
+  );
+};
+export default AdminMain;
