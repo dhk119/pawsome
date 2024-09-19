dd1199e191c25ee142b0bd9f0740cb1ac29db118
aemilius <coline672@gmail.com>
2024-09-19 12:14:25 +0900

    제품 등록 기능 추가

--- pawsome_front/src/components/admin/ProductFrm.js
+++ pawsome_front/src/components/admin/ProductFrm.js
@@ -16,10 +16,8 @@ const ProductFrm = (props) => {
   const setProductPrice = props.setProductPrice;
   const productThumb = props.productThumb;
   const setProductThumb = props.setProductThumb;
-  const productDetail = props.setProductThumb;
+  const productDetail = props.productDetail;
   const setProductDetail = props.setProductDetail;
-  const productRegDate = props.productRegDate;
-  const setProductRegDate = props.setProductRegDate;
   const productShow = props.productShow;
   const setProductShow = props.setProductShow;
   const memberEmail = props.memberEmail;
@@ -34,13 +32,151 @@ const ProductFrm = (props) => {
         setProductImg = reader.result;
       };
     } else {
-      setProductThumb();
-      setProductImg();
+      setProductThumb("");
+      setProductImg("");
     }
   };
   return (
     <div>
-      <div></div>
+      <div>
+        <table className="tbl">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <th>
+                <label htmlFor="productName">제품명</label>
+              </th>
+              <td>
+                <div className="input-item">
+                  <input
+                    type="text"
+                    id="productName"
+                    name="productName"
+                    value={productName}
+                    onChange={setProductName}
+                  ></input>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="productCompany">제품 브랜드</label>
+              </th>
+              <td>
+                <div className="input-item">
+                  <input
+                    type="text"
+                    id="productCompany"
+                    name="productCompany"
+                    value={productCompany}
+                    onChange={setProductCompany}
+                  ></input>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="typeCategory">제품 품종</label>
+              </th>
+              <td>
+                <div className="input-item">
+                  <input
+                    type="text"
+                    id="typeCategory"
+                    name="typeCategory"
+                    value={typeCategory}
+                    onChange={setTypeCategory}
+                  ></input>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="mainCategory">제품 메인 카테고리</label>
+              </th>
+              <td>
+                <div className="input-item">
+                  <input
+                    type="text"
+                    id="mainCategory"
+                    name="mainCategory"
+                    value={mainCategory}
+                    onChange={setMainCategory}
+                  ></input>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="subCategory">제품 서브 카테고리</label>
+              </th>
+              <td>
+                <div className="input-item">
+                  <input
+                    type="text"
+                    id="subCategory"
+                    name="subCategory"
+                    value={subCategory}
+                    onChange={setSubCategory}
+                  ></input>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="productPrice">제품 가격</label>
+              </th>
+              <td>
+                <div className="input-item">
+                  <input
+                    type="text"
+                    id="productPrice"
+                    name="productPrice"
+                    value={productPrice}
+                    onChange={setProductPrice}
+                  ></input>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="productDetail">제품 상세정보</label>
+              </th>
+              <td>
+                <div className="input-item">
+                  <input
+                    type="text"
+                    id="productDetail"
+                    name="productDetail"
+                    value={productDetail}
+                    onChange={setProductDetail}
+                  ></input>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label htmlFor="productShow">제품 등록 여부</label>
+              </th>
+              <td>
+                <div className="input-item">
+                  <input
+                    type="text"
+                    id="productShow"
+                    name="productShow"
+                    value={productShow}
+                    onChange={setProductShow}
+                  ></input>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>제품 등록 관리자 이메일</th>
+              <td>{memberEmail}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
