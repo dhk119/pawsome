20dcc5923568c0032dae061e53a3f17c6cba1218
aemilius <coline672@gmail.com>
2024-09-19 17:35:07 +0900

    문의사항 작성

--- pawsome_front/src/components/inquiry/InquiryFrm.js
+++ pawsome_front/src/components/inquiry/InquiryFrm.js
@@ -1,7 +1,37 @@
 const InquiryFrm = (props) => {
+  const inquiryTitle = props.inquiryTitle;
+  const setInquiryTitle = props.setInquiryTitle;
+  const loginEmail = props.loginEmail;
   return (
     <>
-      <div></div>
+      <div>
+        <div>
+          <table className="tbl">
+            <tbody>
+              <tr>
+                <th>
+                  <label htmlFor="inquiryTitle">제목</label>
+                </th>
+                <td>
+                  <div className="input-item">
+                    <input
+                      type="text"
+                      id="inquiryTitle"
+                      name="inquiryTitle"
+                      value={inquiryTitle}
+                      onChange={setInquiryTitle}
+                    ></input>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <th>작성자</th>
+                <td>{loginEmail}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 };
--- pawsome_front/src/components/inquiry/InquiryWrite.js
+++ pawsome_front/src/components/inquiry/InquiryWrite.js
@@ -2,19 +2,67 @@ import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { loginEmailState } from "../utils/RecoilData";
 import { useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import InquiryFrm from "./InquiryFrm";
+import Quill from "quill";
 
 const InquiryWrite = () => {
   const navigate = useNavigate();
   const backServer = process.env.REACT_APP_BACK_SERVER;
-  const [inquiry, setInquiry] = useState({});
   const [loginEmail, setLoginEmail] = useRecoilState(loginEmailState);
   const [inquiryTitle, setInquiryTitle] = useState("");
   const [inquiryContent, setInquiryContent] = useState("");
   const [inquiryType, setInquiryType] = useState(0);
+  const inputTitle = (e) => {
+    setInquiryTitle(e.target.value);
+  };
+  const inputType = (e) => {
+    setInquiryType(e.target.value);
+  };
+  const writeInquiry = () => {
+    if (inquiryTitle !== "" && inquiryContent !== "" && inquiryType !== 0) {
+      const form = new FormData();
+      form.append("inquiryTitle", inquiryTitle);
+      form.append("inquiryContent", inquiryContent);
+      form.append("memberEmail", loginEmail);
+      axios
+        .post(`${backServer}/inquiry`, form)
+        .then((res) => {
+          console.log(res.data);
+          if (res.data) {
+            navigate("/inquiry/list");
+          } else {
+            Swal.fire({
+              title: "에러발생",
+              text: "에러가 발생했습니다",
+              icon: "error",
+            });
+          }
+        })
+        .catch((err) => {});
+    }
+  };
   return (
-    <>
-      <div></div>
-    </>
+    <section>
+      <div className="page-title">문의사항 작성</div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          writeInquiry();
+        }}
+      >
+        <InquiryFrm
+          inquiryTitle={inquiryTitle}
+          setInquiryTitle={inputTitle}
+          loginEmail={loginEmail}
+        />
+        <div></div>
+        <div className="button-zone">
+          <button type="submit">등록하기</button>
+        </div>
+      </form>
+    </section>
   );
 };
 export default InquiryWrite;
