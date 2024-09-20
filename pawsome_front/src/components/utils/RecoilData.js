f7a036ae786767b23b1b5ef32ed7ad805c4ebbc8
dhk119 <daehean95@gmail.com>
2024-09-20 11:41:52 +0900

    로그인 시 이메일 말고 닉네임으로 받기

--- pawsome_front/src/components/utils/RecoilData.js
+++ pawsome_front/src/components/utils/RecoilData.js
@@ -4,7 +4,10 @@ const loginEmailState = atom({
   key: "loginEmailState",
   default: "test",
 });
-
+const loginNicknameState = atom({
+  key: "loginNicknameStaet",
+  default: "",
+});
 const memberLevelState = atom({
   key: "memberLevelState",
   default: 0,
@@ -15,8 +18,9 @@ const isLoginState = selector({
   get: (state) => {
     const loginEmail = state.get(loginEmailState);
     const memberLevel = state.get(memberLevelState);
+    const loginNickname = state.get(loginNicknameState);
     return loginEmail !== "" && memberLevel !== 0;
   },
 });
 
-export { loginEmailState, memberLevelState, isLoginState };
+export { loginEmailState, memberLevelState, isLoginState, loginNicknameState };
