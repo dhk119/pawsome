af9a38bb05866e8824b27c25567da10032af628f
hiwonhi <dnjsgml0113@gmail.com>
2024-09-22 15:22:45 +0900

    tab 기능 구현

--- pawsome_front/src/components/market/ProductDetail.js
+++ pawsome_front/src/components/market/ProductDetail.js
@@ -1,31 +1,57 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const ProductDetail = () => {
+  const [currentTab, setCurrentTab] = useState(0);
+  const tabArr = [
+    { name: "상세정보", content: "상품 상세정보" },
+    { name: "상품평", content: "상품 리뷰" },
+    { name: "상품문의", content: "상품 Q&A" },
+    { name: "주문/배송 안내", content: "주문/배송 안내" },
+  ];
+  const selectTapHandler = (index) => {
+    setCurrentTab(index);
+  };
   return (
-    <>
-      <div className="productDetail-wrap">
-        <div className="productDetail">
-          <div className="product-thumb">
-            <img src="/image/basicimage.png" />
+    <div className="productDetail-wrap">
+      <div className="productDetail">
+        <div className="product-thumb">
+          <img src="/image/basicimage.png" />
+        </div>
+        <div className="product-info">
+          <div className="product-category">
+            <Link to="#">전체</Link>
+            <span> {">"} </span>
+            <Link to="#">사료</Link>
           </div>
-          <div className="product-info">
-            <div className="product-category">
-              <Link to="#">전체</Link>
-              <span> {">"} </span>
-              <Link to="#">사료</Link>
-            </div>
-            <div className="product-name">상품명</div>
-            <div className="product-price">상품금액</div>
-            <div className="product-amount">수량</div>
-            <div className="product-totalprice">총 금액</div>
-            <div className="product-btn">
-              <button>장바구니 담기</button>
-              <button>바로 구매</button>
-              <button>관심상품</button>
-            </div>
+          <div className="product-name">상품명</div>
+          <div className="product-price">상품금액</div>
+          <div className="product-amount">수량</div>
+          <div className="product-totalprice">총 금액</div>
+          <div className="product-btn">
+            <button>장바구니 담기</button>
+            <button>바로 구매</button>
+            <button>관심상품</button>
           </div>
         </div>
-        <div className="detail-menu-wrap">
+      </div>
+      <div className="detail-menu-wrap">
+        {tabArr.map((item, index) => {
+          return (
+            <li
+              key={index}
+              className={currentTab === index ? "tab active-tab" : "tab"}
+              onClick={() => selectTapHandler(index)}
+            >
+              {item.name}
+            </li>
+          );
+        })}
+      </div>
+      <div className="tab-content">
+        <span>{tabArr[currentTab].content}</span>
+      </div>
+      {/*
           <div className="tab">
             <div>상세정보</div>
             <div>상품평</div>
@@ -46,9 +72,8 @@ const ProductDetail = () => {
               <spna>주문/배송 안내</spna>
             </div>
           </div>
-        </div>
-      </div>
-    </>
+          */}
+    </div>
   );
 };
 
