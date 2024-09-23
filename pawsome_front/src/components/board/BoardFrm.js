06bb6aed2f3b551ceb05186b6b2b3915efdb996e
ljhsky0523 <>
2024-09-23 15:32:19 +0900

    오산완 글쓰기 지도 커밋

--- pawsome_front/src/components/board/BoardFrm.js
+++ pawsome_front/src/components/board/BoardFrm.js
@@ -1,6 +1,9 @@
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import Map from "../board/Map";
+import WalkMap from "./WalkMap";
+
 const BoardFrm = (props) => {
   const backServer = process.env.REACT_APP_BACK_SERVER;
   const boardTag = props.boardTag;
@@ -32,6 +35,7 @@ const BoardFrm = (props) => {
     setBoardFile([...boardFile, ...fileArr]);
     setShowBoardFile([...showBoardFile, ...filenameArr]);
   };
+
   return (
     <div className="board-writeFrm">
       <div className="writeFrm-tag">
@@ -116,9 +120,35 @@ const BoardFrm = (props) => {
             ></input>
           </li>
           <li>
+            {boardTag == "#오산완" && (
+              <div id="map-box" style={{ textAlign: "center" }}>
+                <span>
+                  * 현재 위치가 나오지 않으면 위치 사용을 허용으로 변경해주세요.
+                  *
+                </span>
+                <div
+                  id="walkMap"
+                  style={{
+                    width: "500px",
+                    height: "400px",
+                  }}
+                >
+                  <WalkMap />
+                </div>
+                <span style={{ fontSize: "12px" }}>
+                  지도를 마우스로 클릭하면 선 그리기가 시작되고 오른쪽 마우스를
+                  클릭하면 선 그리기가 종료됩니다
+                </span>
+                <br></br>
+                <span>
+                  * 산책코스를 그려 지도를 캡쳐 후 이미지를 첨부해주세요! *
+                </span>
+              </div>
+            )}
+
             <div className="board-img-wrap">
               <label htmlFor="boardFile">
-                <img src="/image/default_img.png" />
+                <img src="/image/default_img.png" alt="Upload" />
               </label>
               <input
                 className="img-file"
