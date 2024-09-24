72898574b46c826f466d31b7b4bbc1b9b9c9f884
dhk119 <daehean95@gmail.com>
2024-09-24 21:16:53 +0900

    게시글 상세보기 진행중

--- pawsome_front/src/components/board/BoardView.js
+++ pawsome_front/src/components/board/BoardView.js
@@ -1,20 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BoardNav from "./BoardNav";
 import { useParams } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { memberNicknameState } from "../utils/RecoilData";
+import axios from "axios";
+import * as DOMPurify from "dompurify";
 
 const BoardView = () => {
+  const backServer = process.env.REACT_APP_BACK_SERVER;
   const params = useParams();
   const boardNo = params.boardNo;
+  const [board, setBoard] = useState({});
+  const [memberNickname, setMemberNickname] =
+    useRecoilState(memberNicknameState);
+  const [member, setMember] = useState({});
+  useEffect(() => {
+    axios
+      .get(`${backServer}/board/boardNo/${boardNo}`)
+      .then((res) => {
+        console.log(res);
+        setBoard(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <section className="section board-view-wrap">
-      <BoardNav />
       <div className="board-view-content">
-        <div></div>
+        <div>
+          {board.boardTag} {board.boardTitle}
+        </div>
+        <div>
+          {member.memberProfile}
+          {board.memberNickname}
+          {board.regDate}
+          {board.readCount}
+          {/* {boardLike} */}
+        </div>
+        <div>
+          <img src={`${backServer}/board/${board.boardThumb}`}></img>
+          {board.boardContent && (
+            <div
+              style={{
+                width: "60vw",
+                whiteSpace: "normal",
+              }}
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(String(board.boardContent)),
+              }}
+            />
+          )}
+        </div>
       </div>
       <div className="other-board-content"></div>
       <div className="reply-wrap"></div>
     </section>
   );
 };
-
+const moreView = () => {};
 export default BoardView;
