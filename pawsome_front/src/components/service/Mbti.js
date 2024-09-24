a2578c68a33129780e238806873c380858db2aa9
ljhsky0523 <ljhsky0523@naver.com>
2024-09-24 15:36:23 +0900

    사진추가

--- pawsome_front/src/components/service/Mbti.js
+++ pawsome_front/src/components/service/Mbti.js
@@ -1,10 +1,21 @@
+import { Link } from "react-router-dom";
 const Mbti = () => {
   return (
     <div className="mbti-container">
+      <Link to="/service/petService" style={{ color: "#ffa518" }}>
+        메인으로
+      </Link>
       <div className="dog-mbti">
-        <h2>12문제로 알아보는 나의 강아지 유형 MBTI는? 테스트로 알아보자!</h2>
-        <img src="/image/dddd.png" style={{ width: "700px" }} />
-        <div className="start-btn">테스트 시작하기</div>
+        <h2 style={{ margin: "20px" }}>
+          12문제로 알아보는 나의 강아지 유형 MBTI는? 테스트로 알아보자!
+        </h2>
+        <img src="/image/service/mbti-main.jpg" style={{ width: "600px" }} />
+        <div
+          className="start-btn"
+          style={{ margin: "40px", fontSize: "x-large" }}
+        >
+          테스트 시작하기
+        </div>
       </div>
     </div>
   );
--- pawsome_front/src/components/service/RecordList.js
+++ pawsome_front/src/components/service/RecordList.js
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
 const RecordList = () => {
-  return <h1>반려동물 등록조회</h1>;
+  return (
+    <div className="container">
+      <Link to="/service/petService" style={{ color: "#ffa518" }}>
+        메인으로
+      </Link>
+      <div className="service-wrap">
+        <h1>반려동물 등록조회</h1>
+      </div>
+    </div>
+  );
 };
 
 export default RecordList;
