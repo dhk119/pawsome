01cab757160e4646e4afe1c8986630902cfb192f
aemilius <coline672@gmail.com>
2024-10-04 16:49:37 +0900

    펫차트

--- pawsome_front/src/components/admin/Admin.js
+++ pawsome_front/src/components/admin/Admin.js
@@ -34,7 +34,7 @@ const Admin = () => {
           </div>
           <div className="admin-link-gap">
             <Link className="admin-product-label" to="/admin/petChart">
-              판매 차트
+              펫 차트
             </Link>
           </div>
         </div>
--- pawsome_front/src/components/admin/PetChart.js
+++ pawsome_front/src/components/admin/PetChart.js
@@ -1,4 +1,3 @@
-import { Tooltip } from "bootstrap";
 import { useState } from "react";
 import {
   Bar,
@@ -33,7 +32,6 @@ const PetChart = () => {
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis />
-        <Tooltip />
         <Legend />
         <Bar dataKey="pv" fill="#8884d8" />
         <Bar dataKey="uv" fill="#82ca9d" />
