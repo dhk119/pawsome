43ab9941d2ab8ba7b0646e1168f8c6dfd4bc3e76
ljhsky0523 <ljhsky0523@naver.com>
2024-10-08 18:22:14 +0900

    건강테스트 저장 및 업데이트 완료

--- pawsome_back/src/main/java/kr/co/iei/pet/controller/PetController.java
+++ pawsome_back/src/main/java/kr/co/iei/pet/controller/PetController.java
@@ -7,10 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.iei.member.model.dto.PetDTO;
+
+import kr.co.iei.pet.dto.SaveStatusDTO;
 import kr.co.iei.pet.service.PetService;
 
 @CrossOrigin
@@ -26,4 +30,10 @@ public class PetController {
 		List<PetDTO> petList = petService.selectPetList(memberEmail);
 		return ResponseEntity.ok(petList);	
 	}
+	
+	@PostMapping("/saveStatus")
+	public ResponseEntity<Integer> savePetStatus(@RequestBody SaveStatusDTO saveResultStatus){
+		int saveResult = petService.savePetStatus(saveResultStatus);
+	return ResponseEntity.ok(saveResult);
+}
 }
