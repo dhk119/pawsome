486e34bfcd927ca70676e2d1ce232fb6baa05b7b
ljhsky0523 <ljhsky0523@naver.com>
2024-10-09 19:32:00 +0900

    기록보기 끝~

--- pawsome_front/src/components/service/HealthTestRecord.js
+++ pawsome_front/src/components/service/HealthTestRecord.js
@@ -1,121 +1,208 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import html2canvas from "html2canvas";
+import Swal from "sweetalert2";
 import { useRecoilState } from "recoil";
 import { loginEmailState } from "../utils/RecoilData";
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts";
-import { Link } from "react-router-dom"; // Link 임포트
-import html2canvas from "html2canvas";
-import Swal from "sweetalert2";
-
-// 필요한 헬퍼 함수 정의
-const getHealthStatus = (score) => {
-  if (score <= 20) return "매우 위험";
-  if (score <= 40) return "위험";
-  if (score <= 60) return "주의";
-  if (score <= 80) return "양호";
-  return "건강";
-};
-
-const getHealthStatusColor = (status) => {
-  switch (status) {
-    case "매우 위험":
-      return "#ff0000";
-    case "위험":
-      return "#ff9999";
-    case "주의":
-      return "#ffcc66";
-    case "양호":
-      return "#66cc66";
-    case "건강":
-      return "#5799ff";
-    default:
-      return "#ffffff"; // 기본 색상
-  }
-};
+import { Link } from "react-router-dom";
 
 const HealthTestRecord = () => {
   const [sessionPets, setSessionPets] = useState([]);
   const [selectedPet, setSelectedPet] = useState(null);
+  const [selectedPetNo, setSelectedPetNo] = useState(null);
   const backServer = process.env.REACT_APP_BACK_SERVER;
   const [memberEmail] = useRecoilState(loginEmailState);
   const [petSelectComplete, setPetSelectComplete] = useState(false);
-  const [formData, setFormData] = useState({
-    no: "",
-    name: "",
-  });
-  const [data, setData] = useState([]); // 데이터 상태 추가
-  const [finalScores, setFinalScores] = useState({}); // 최종 점수 상태 추가
-  const [healthLabels, setHealthLabels] = useState({}); // 건강 라벨 상태 추가
-  const [healthDescriptions, setHealthDescriptions] = useState({}); // 건강 설명 상태 추가
   const [selectedWeightManagementTip, setSelectedWeightManagementTip] =
-    useState(""); // 몸무게 관리 팁 상태 추가
+    useState("");
+  const [finalScores, setFinalScores] = useState({
+    skin: 0,
+    dental: 0,
+    bone: 0,
+    eye: 0,
+    heart: 0,
+    immunity: 0,
+  });
 
-  const petDataSelection = (pet) => {
-    setPetSelectComplete(true);
-    setSelectedPet(pet.petName);
-    setFormData({
-      name: pet.name,
-      no: pet.petNo,
-    });
-    // 여기에 데이터와 점수를 가져오는 로직 추가
-    fetchPetHealthData(pet.petNo);
+  const getHealthStatus = (score) => {
+    if (score <= 20) return "매우 위험 단계!";
+    if (score <= 40) return "위험 단계!";
+    if (score <= 60) return "주의 단계!";
+    if (score <= 80) return "양호";
+    return "건강";
   };
+
+  const healthDescriptions = {
+    skin: {
+      "매우 위험 단계!":
+        "피부 상태가 매우 좋지 않으며 지금 당장 수의사와의 상담이나 치료가 필요합니다! 가까운 동물병원에 당장 방문하세요.",
+      "위험 단계!":
+        "피부 상태가 좋지 않습니다. 정기적인 검진이나 치료가 필요합니다. 가까운 동물병원에 방문해주세요.",
+      "주의 단계!":
+        "피부 상태가 조금 좋지 않습니다! 동물병원을 방문하거나 관리를 꾸준히 해주세요.",
+      양호: "피부 상태가 양호합니다. 계속 관심을 가져주세요.",
+      건강: "피부 상태가 아주 건강합니다!!",
+    },
+    dental: {
+      "매우 위험 단계!":
+        "치아 상태가 매우 좋지 않으며 지금 당장 수의사와의 상담이나 치료가 필요합니다! 가까운 동물병원에 당장 방문하세요.",
+      "위험 단계!":
+        "치아 상태가 좋지 않습니다. 정기적인 검진이나 치료가 필요합니다. 가까운 동물병원에 방문해주세요.",
+      "주의 단계!":
+        "치아 상태가 조금 좋지 않습니다! 동물병원을 방문하거나 관리를 꾸준히 해주세요.",
+      양호: "치아 상태가 양호합니다. 계속 관심을 가져주세요.",
+      건강: "치아 상태가 아주 건강합니다!!",
+    },
+    bone: {
+      "매우 위험 단계!":
+        "뼈가 매우 좋지 않으며 지금 당장 수의사와의 상담이나 치료가 필요합니다! 가까운 동물병원에 당장 방문하세요.",
+      "위험 단계!":
+        "뼈가 좋지 않습니다. 정기적인 검진이나 치료가 필요합니다. 가까운 동물병원에 방문해주세요.",
+      "주의 단계!":
+        "뼈가 조금 좋지 않습니다! 동물병원을 방문하거나 관리를 꾸준히 해주세요.",
+      양호: "뼈가 양호합니다. 계속 관심을 가져주세요.",
+      건강: "뼈가 아주 건강합니다!!",
+    },
+    eye: {
+      "매우 위험 단계!":
+        "눈이 매우 좋지 않으며 지금 당장 수의사와의 상담이나 치료가 필요합니다! 가까운 동물병원에 당장 방문하세요.",
+      "위험 단계!":
+        "눈 상태가 좋지 않습니다. 정기적인 검진이나 치료가 필요합니다. 가까운 동물병원에 방문해주세요.",
+      "주의 단계!":
+        "눈이 조금 좋지 않습니다! 동물병원을 방문하거나 관리를 꾸준히 해주세요.",
+      양호: "눈이 양호합니다. 계속 관심을 가져주세요.",
+      건강: "눈이 아주 건강합니다!!",
+    },
+    heart: {
+      "매우 위험 단계!":
+        "심장 상태가 매우 좋지 않으며 지금 당장 수의사와의 상담이나 치료가 필요합니다! 가까운 동물병원에 당장 방문하세요.",
+      "위험 단계!":
+        "심장 상태가 좋지 않습니다. 정기적인 검진이나 치료가 필요합니다. 가까운 동물병원에 방문해주세요.",
+      "주의 단계!":
+        "심장 상태가 조금 좋지 않습니다! 동물병원을 방문하거나 관리를 꾸준히 해주세요.",
+      양호: "심장 상태가 양호합니다. 계속 관심을 가져주세요.",
+      건강: "심장 상태가 아주 건강합니다!!",
+    },
+    immunity: {
+      "매우 위험 단계!":
+        "면역력이나 호흡기가 매우 좋지 않으며 지금 당장 수의사와의 상담이나 치료가 필요합니다! 가까운 동물병원에 당장 방문하세요.",
+      "위험 단계!":
+        "면역력이나 호흡기가 좋지 않습니다. 정기적인 검진이나 치료가 필요합니다. 가까운 동물병원에 방문해주세요.",
+      "주의 단계!":
+        "면역력이나 호흡기가 조금 좋지 않습니다! 안좋은곳을 파악하여 관리를 꾸준히 해주세요.",
+      양호: "면역력이나 호흡기가 양호합니다. 계속 관리하세요.",
+      건강: "면역력이나 호흡기가 아주 건강합니다!!",
+    },
+  };
+
   useEffect(() => {
     axios
       .get(`${backServer}/pet/petList/${memberEmail}`)
       .then((res) => {
         setSessionPets(res.data);
+        console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [backServer]);
-  const fetchPetHealthData = async (petNo) => {
-    try {
-      const response = await axios.get(`${backServer}/pet/health/${petNo}`);
-      setData(response.data.data); // 그래프 데이터
-      setFinalScores(response.data.finalScores);
-      setHealthLabels(response.data.healthLabels);
-      setHealthDescriptions(response.data.healthDescriptions);
-      setSelectedWeightManagementTip(response.data.weightManagementTip);
-    } catch (error) {
-      console.error("Error fetching pet health data:", error);
-    }
-  };
-
-  useEffect(() => {
-    const fetchSessionPets = async () => {
-      try {
-        const response = await axios.get(
-          `${backServer}/pet/petList/${memberEmail}`
-        );
-        setSessionPets(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchSessionPets();
   }, [backServer, memberEmail]);
 
+  useEffect(() => {
+    if (selectedPetNo) {
+      axios
+        .get(`${backServer}/pet/healthResult/${selectedPetNo}`)
+        .then((res) => {
+          console.log(res.data);
+          if (!res.data || Object.keys(res.data).length === 0) {
+            // 기록이 없을 경우 알림 띄우기
+            return Swal.fire({
+              icon: "warning",
+              title: "알림",
+              text: "이 반려동물의 건강 기록이 없습니다. 먼저 검사를 진행해주세요.",
+            });
+          }
+          const healthInfo = {
+            skin: res.data.petSkinStatus,
+            dental: res.data.petDentalStatus,
+            bone: res.data.petBoneStatus,
+            eye: res.data.petEyeStatus,
+            heart: res.data.petHeartStatus,
+            immunity: res.data.petImmunityStatus,
+          };
+          setSelectedWeightManagementTip(
+            weightManagementTips[res.data.petWeightStatus]
+          );
+          setFinalScores(healthInfo);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [selectedPetNo]);
+
+  const healthLabels = {
+    skin: "피부",
+    dental: "치아",
+    bone: "뼈",
+    eye: "눈",
+    heart: "심장",
+    immunity: "면역력",
+  };
+
+  const petDataSelection = (pet) => {
+    setPetSelectComplete(true);
+    setSelectedPet(pet.petName);
+    setSelectedPetNo(pet.petNo);
+  };
+  const getHealthStatusColor = (status) => {
+    switch (status) {
+      case "매우 위험 단계!":
+        return "#ff0000";
+      case "위험 단계!":
+        return "#ff9999";
+      case "주의 단계!":
+        return "#ffcc66";
+      case "양호":
+        return "#66cc66";
+      case "건강":
+        return "#5799ff";
+      default:
+        return "#fff";
+    }
+  };
+  const weightManagementTips = {
+    1: "체중이 약간 마른 상태입니다. 적절한 영양 공급과 함께 건강한 체중을 유지하세요.",
+    2: "체중이 정상입니다. 꾸준한 운동과 균형 잡힌 식사를 유지하세요.",
+    3: "체중이 약간 살찐 상태입니다. 다이어트를 고려해보세요.",
+    4: "체중이 많이 살찐 상태입니다. 전문가의 도움을 받는 것이 좋습니다.",
+  };
   const onClickDownloadButton = () => {
     const target = document.getElementById("result");
     if (!target) {
-      return alert("사진 저장에 실패했습니다.");
+      return Swal.fire({
+        icon: "error",
+        title: "실패!",
+        text: "사진 저장에 실패했습니다.",
+      });
     }
     html2canvas(target).then((canvas) => {
       const link = document.createElement("a");
       document.body.appendChild(link);
       link.href = canvas.toDataURL("image/png");
-      link.download = "HealthTest.png"; // 다운로드 이미지 파일 이름
+      link.download = "HealthResult.png";
       link.click();
       document.body.removeChild(link);
     });
   };
-
+  const data = Object.keys(finalScores).map((key) => ({
+    name: healthLabels[key],
+    score: finalScores[key],
+  }));
   return (
     <div className="HT-container">
       {!petSelectComplete ? (
-        <div>
+        <div className="service-wrap">
           <p style={{ fontSize: "x-large", fontWeight: "bold" }}>
             기록을 확인할 반려동물을 선택해주세요!
           </p>
@@ -139,9 +226,10 @@ const HealthTestRecord = () => {
           </select>
         </div>
       ) : (
-        <div>
+        <div className="service-wrap">
           <div className="result-box" id="result">
             <h1>{selectedPet}의 건강테스트 결과</h1>
+
             <BarChart
               width={700}
               height={400}
@@ -152,8 +240,8 @@ const HealthTestRecord = () => {
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="name" />
               <YAxis
-                domain={[20, 100]}
-                ticks={[15, 35, 55, 80, 105]}
+                domain={[20, 100]} // y축 범위
+                ticks={[15, 35, 55, 80, 105]} // y축에 표시할 점수
                 tickFormatter={(value) => {
                   if (value <= 20) return "매우 위험";
                   if (value <= 40) return "위험";
@@ -169,16 +257,19 @@ const HealthTestRecord = () => {
                 shape={(props) => {
                   const { score } = props;
                   let fillColor;
-                  if (score <= 20) fillColor = "#ff0000";
-                  else if (score <= 40) fillColor = "#ff9999";
-                  else if (score <= 60) fillColor = "#ffcc66";
-                  else if (score <= 80) fillColor = "#66cc66";
-                  else fillColor = "#5799ff";
+                  if (score <= 20) fillColor = "#ff0000"; // 매우 위험
+                  else if (score <= 40) fillColor = "#ff9999"; // 위험
+                  else if (score <= 60) fillColor = "#ffcc66"; // 주의
+                  else if (score <= 80) fillColor = "#66cc66"; // 양호
+                  else fillColor = "#5799ff"; // 건강
                   return <rect {...props} fill={fillColor} />;
                 }}
                 label={{ position: "top" }}
               />
             </BarChart>
+            <p className="weight-status">
+              몸무게 관리 방법 : {selectedWeightManagementTip}
+            </p>
             {Object.keys(finalScores).map((key) => (
               <div
                 key={key}
@@ -197,6 +288,7 @@ const HealthTestRecord = () => {
                 </div>
               </div>
             ))}
+
             <div className="color-squares">
               <div
                 className="square"
@@ -230,16 +322,18 @@ const HealthTestRecord = () => {
               </div>
             </div>
             <div>
-              <span>
+              <span style={{ color: "red", fontWeight: "bold" }}>
                 매우 위험이나 위험이 나왔으면 꼭 가까운 동물 병원에 바로
                 연락해보세요!
               </span>
-              <Link to="/service/allMap"> -주변 동물병원 검색하기-</Link>
+              <br />
+              <button className="map-search-btn">
+                <Link to="/service/allMap" style={{ color: "#5799ff" }}>
+                  {" "}
+                  -주변 동물병원 검색하기-
+                </Link>
+              </button>
             </div>
-            <p className="weight-status">
-              몸무게 관리 방법: {selectedWeightManagementTip}
-            </p>
-            <br />
           </div>
           <button className="ps-btn2" onClick={onClickDownloadButton}>
             앨범에 저장
